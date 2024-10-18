4aeffd2b4323d82000d964147f851fc1ec58791a
Ana Carolina <anacarolinacostaabbud@gmail.com>
2024-10-17 22:28:42 -0300

    Authentication css

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -1,51 +1,49 @@
 @extends('authentication.layout')
 @section('title', 'Login')
 @section('content')
-
-    <div class="overlay"></div>
-        <a href="{{route('index')}}"><img class="homeImg" src="../Assets/Home.png" alt=""></a>
+<div class="overlay"></div>
+<a href="{{route('index')}}"><img class="homeImg" src="../Assets/Home.png" alt=""></a>
+<div class="main">
+    <div class="window">
+        <div class="containerh1">
+            <img src="../Assets/BlackLogo.png" alt="">
+            <h1>OCELOT</h1>
+        </div>
+        <h2>ALWAYS FORWARD</h2>
         <div class="">
             @if($errors->any())
-                <div class="col-12">
-                    @foreach($errors->all() as $error)
-                        <div class="alert alert-danger">{{$error}}</div>
-                    @endforeach
-                </div>
+            <div class="col-12">
+                @foreach($errors->all() as $error)
+                <div class="alert alert-danger">{{$error}}</div>
+                @endforeach
+            </div>
             @endif
 
             @if(session()->has('error'))
-                <div class="alert alert-danger">{{session('error')}}</div>
+            <div class="alert alert-danger">{{session('error')}}</div>
             @endif
 
             @if(session()->has('success'))
-                <div class="alert alert-success">{{session('success')}}</div>
+            <div class="alert alert-success">{{session('success')}}</div>
             @endif
         </div>
-        <div class="main">
-            <div class="window">
-            <div class="containerh1">
-                <img src="../Assets/BlackLogo.png" alt="">
-                <h1>OCELOT</h1>
-            </div>
-            <h2>ALWAYS FORWARD</h2>
-            
-<div class="form-container">
-                    <form action="{{route('login.post')}}" method="POST" class="ms-auto me-auto mt-3">
-                        @csrf
-                        <div class="input-box">
-                            <img src="../Assets/Email.png" alt="">
-                            <input type="email" class="form-control" name="email" placeholder="Email">
-                        </div>
-                        <div class="input-box">
-                            <img src="../Assets/Password.png" alt="">
-                            <input type="password" class="form-control" name="password" placeholder="Senha">
-                        </div>
-                        <div class="divsubmit">
-                            <input type="submit" value="Submit">
-                        </div>  
-                    </form>
+        <div class="form-container">
+            <form action="{{route('login.post')}}" method="POST" class="ms-auto me-auto mt-3">
+                @csrf
+                <div class="input-box">
+                    <img src="../Assets/Email.png" alt="">
+                    <input type="email" class="form-control" name="email" placeholder="Email">
                 </div>
-            </div>
+                <div class="input-box">
+                    <img src="../Assets/Password.png" alt="">
+                    <input type="password" class="form-control" name="password" placeholder="Senha">
+                </div>
+                <div class="divsubmit">
+                    <input type="submit" value="Submit">
+                </div>
+            </form>
         </div>
     </div>
+</div>
+</div>
 @endsection
