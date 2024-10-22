683e93d00a3332827fc8fc2ac65e7d2d6544c2d2
GABRIEL GARCIA AGUIAR <0031432312024@lti.fatecso.edu.br>
2024-10-22 14:20:37 -0300

    Ocelot Logo addet at home screen

--- resources/views/shoe/header.blade.php
+++ resources/views/shoe/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{ route('index') }}">OCELOT</a>
+    <a class="navbar-brand" href="{{ route('index') }}"><img src="{{asset('assets/BlackLogo.png')}}" alt="" style="width: 45px; height: 45px;"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
