cb6dd4dfe77f6cbd95a2320637abfdddfadcc5de
Gabriel Aguiar <ggaxmail@gmail.com>
2024-11-06 11:40:14 -0300

    Header minor changes

--- resources/views/user/header.blade.php
+++ resources/views/user/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">OCELOT</a>
+    <a class="navbar-brand" href="{{ route('index') }}"><img src="{{asset('assets/BlackLogo.png')}}" alt="" style="width: 45px; height: 45px;"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,52 +10,34 @@
           <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Liactionnk</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="{{ route('users') }}">Users</a>
         </li>
 
-        <!-- Link para a lista de usuários -->
-        <li class="nav-item ms-1"> 
-          <a href="{{ route('users') }}">
-            <img src="{{ asset('assets/Users.png') }}" alt="Usuário" class="img-fluid" style="width: 40px; height: 40px;">
-          </a>
-        </li>
       </ul>
 
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <!-- Formulário de busca -->
         <li class="nav-item mb-2">
-          <form class="d-flex" role="search" action="{{ route('search') }}" method="GET">
+          <form class="d-flex"   role="search" action="{{ route('search') }}" method="GET">
             <input class="form-control me-2" type="search" name="query" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <button class="btn btn-outline-success" type="submit" style="border-color: black; color: black; transition: all 0.2s ease-in-out;" onmouseover="this.style.backgroundColor='white'" onmouseout="this.style.backgroundColor='transparent'">Search</button>
           </form>
         </li>
-
-        <!-- Link para o perfil do usuário -->
+        
         <li class="nav-item ms-1"> 
           <a href="{{ route('user') }}">
             <img src="{{ asset('assets/DarkUser.png') }}" alt="Usuário" class="img-fluid" style="width: 40px; height: 40px;">
           </a>
         </li>
 
-        <!-- Link para logout -->
+        
         <li class="nav-item">
           <a class="nav-link" href="{{ route('logout') }}">Logout</a>
         </li>
       </ul>
+
+
+
+      
     </div>
   </div>
 </nav>
