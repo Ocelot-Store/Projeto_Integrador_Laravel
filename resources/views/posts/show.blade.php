cbec78441fc58465a989afe945fa3d4c899edd41
Gabriel Aguiar <ggaxmail@gmail.com>
2024-12-09 01:54:28 -0300

    Comments css updated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,8 +2,8 @@
 
 @section('title', 'Comunidade')
 
-@section('style')  
-<link rel="stylesheet" href="{{ asset('css/posts/comments.css') }}"> 
+@section('style')
+<link rel="stylesheet" href="{{ asset('css/posts/comments.css') }}">
 @endsection
 
 @section('content')
@@ -15,6 +15,7 @@
             <img class="user-image" src="{{ $post->user->profileImage ? asset('storage/' . $post->user->profileImage) : asset('assets/DarkUser.png') }}" alt="Imagem de Perfil">
             <div>
                 <strong>{{ $post->user->name }}</strong>
+                <p class="post-time">{{ $post->created_at->setTimezone('America/Sao_Paulo')->format('d/m/Y H:i') }}</p>
             </div>
         </div>
 
@@ -42,25 +43,38 @@
         @endif
 
         <!-- Data e hora do post -->
-        <p class="post-time">{{ $post->created_at->setTimezone('America/Sao_Paulo')->format('d/m/Y H:i') }}</p>
+        
     </div>
 
     <!-- Formulário de comentário -->
     <div class="comment-section">
         <form action="{{ route('posts.comment.store', $post->id) }}" method="POST" class="comment-form">
             @csrf
-            <textarea name="content" rows="3" placeholder="Adicionar um comentário..." required></textarea>
+            <div style="display: flex; gap: 5px; margin-bottom: 5px;">
+                <img src="{{ Auth::user()->profileImage ? asset('storage/' . Auth::user()->profileImage) : asset('assets/DarkUser.png') }}" class="user-image" alt="">
+                <textarea name="content" rows="3" placeholder="Adicionar um comentário..." required></textarea>
+            </div>
             <button type="submit">Comentar</button>
         </form>
 
         <!-- Lista de comentários -->
+
         <h3>{{ $post->comments->count() }} Comentários</h3>
         <div class="comments-list">
             @foreach ($post->comments as $comment)
-                <div class="comment">
-                    <p class="text-muted">- {{ $comment->user->name }} ({{ $comment->created_at->setTimezone('America/Sao_Paulo')->format('d/m/Y H:i') }})</p>
-                    <p>{{ $comment->comment }}</p>
+            <div class="comment">
+                <div class="comment-info" >
+                    <img class="user-image" src="{{ $post->user->profileImage ? asset('storage/' . $post->user->profileImage) : asset('assets/DarkUser.png') }}" style="min-width: 50px;" alt="Imagem de Perfil">
+                    <div style="display: flex; flex-direction: column; margin-top: 10px;">
+                        <div style="display: flex; align-items: center; margin-bottom: 10px;">
+                            <strong style="margin-right: 5px;">- {{ $comment->user->name }} </strong>
+                            <p style="margin:0;" >({{ $comment->created_at->setTimezone('America/Sao_Paulo')->format('d/m/Y H:i') }})</p>
+                        </div>
+                        <p>{{ $comment->comment }}</p>
+                    </div>
                 </div>
+                
+            </div>
             @endforeach
         </div>
     </div>
