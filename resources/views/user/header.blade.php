a6cc9984a9f98a2b615a1f5c9a58200f401c8266
Ana Carolina <anacarolinacostaabbud@gmail.com>
2024-12-10 18:45:56 -0300

    Changing more screens to PT-BR and redirecting registration to home screen

--- resources/views/user/header.blade.php
+++ resources/views/user/header.blade.php
@@ -7,10 +7,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+          <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Início</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ route('users') }}">Users</a>
+          <a class="nav-link" href="{{ route('users') }}">Usuários</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('posts.index') }}">Comunidade</a>
